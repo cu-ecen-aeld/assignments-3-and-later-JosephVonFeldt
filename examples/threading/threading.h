4f23048ee39c67638fd0422069ceed80ad189740
Joseph VonFeldt <joey.vonfeldt@gmail.com>
2024-02-13 18:12:36 -0700

    Thread stuff

--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -19,6 +19,9 @@ struct thread_data{
      * Set to true if the thread completed with success, false
      * if an error occurred.
      */
+    int waitBefore;
+    int waitHolding;
+    pthread_mutex_t *mutex;
     bool thread_complete_success;
 };
 
