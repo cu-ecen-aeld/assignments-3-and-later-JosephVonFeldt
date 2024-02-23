6571c379a2be85223dfa1db7f6b738e99fc239fe
Joseph VonFeldt <joey.vonfeldt@gmail.com>
2024-02-23 02:56:24 -0700

    Change for improved consistency.

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -12,58 +12,37 @@
 #include <unistd.h>
 #include <signal.h>
 #define PORT 9000
-#define BUFFSIZE 1024
 
 int socketfd;
 int recieve(int acceptedfd){
-    char buff[BUFFSIZE];
+    char c;
     FILE *file  = fopen("/var/tmp/aesdsocketdata", "a+");
     int end = 0;
-    while (recv(acceptedfd, buff, BUFFSIZE,0 )>0){
-        for (int i=0; i<BUFFSIZE; i++){
-            if (buff[i]==0){
-                break;
-            }
-            else if (buff[i]=='\n' || buff[i]=='\r'){
-                fputc(buff[i], file);
-                end=1;
-                break;
-            }else{
-                fputc(buff[i], file);
-            }
+    while (recv(acceptedfd, &c, sizeof (c),0 )>0){
+        if (c == '\n'){
+            end = 1;
         }
-        strcpy(buff, "");
+        fputc(c, file);
         if (end){
             break;
         }
-//        if (buff[strlen(buff)-1] == '\n'){
-//            break;
-//        }
-//
     }
     if (end==0){
-        fputs("\n", file);
+        fputc('\n', file);
     }
     fclose(file);
     return 0;
 }
 
 int sendback(int acceptedfd){
-    char buff[BUFFSIZE];
+    char c;
     FILE *file  = fopen("/var/tmp/aesdsocketdata", "r");
-    int x = fread(buff, 1, BUFFSIZE, file);
+    int x = fread(&c, 1, sizeof(c), file);
     while (x > 0){
-        //printf("%s", buff);
-        send(acceptedfd, buff, x, 0);
-        strcpy(buff, "");
-        x = fread(buff, 1, BUFFSIZE, file);
+        send(acceptedfd, &c, sizeof (c), 0);
+        x = fread(&c, 1, sizeof (c), file);
     }
 
-    if (strlen(buff)){
-        //printf("\t%s", buff);
-        send(acceptedfd, buff, strlen(buff), 0);
-    }
-//while (recv())
     fclose(file);
     return 0;
 }
@@ -83,7 +62,6 @@ int main(int argc, char **argv){
     openlog(NULL, 0, LOG_USER);
     signal(SIGTERM, handleSig);
     signal(SIGINT, handleSig);
-    char buff[BUFFSIZE];
     struct sockaddr_in address;
     address.sin_family = PF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
